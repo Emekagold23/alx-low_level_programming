ac74bb49a1147c4a34a622e358bea9b8b13087e7
Emekagold23 <onyekachigold23@gmail.com>
2023-08-05 12:27:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: always 0
+ */
+
+int main (int __attribute__((unused)) argc, char *argv[])
+
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
