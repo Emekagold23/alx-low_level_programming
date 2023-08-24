63cf12568874b9b2a3e6ff72c1bc9850f6141643
Emekagold23 <onyekachigold23@gmail.com>
2023-08-24 16:45:22 -0700

    firstEmekagold

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void firstEmekagold(void)__attribute__((constructor));
+/**
+ * firstEmekagold - funtion that prints before the main function
+ */
+
+void firstEmekagold(void)
+
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
