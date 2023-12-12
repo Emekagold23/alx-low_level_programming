f90fb0ca368caecb977c7bb28239bf9110b8fcc3
Emekagold23 <onyekachigold23@gmail.com>
2023-12-12 07:49:55 -0800

    dynamic library that contains C functions

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted result
+ */
+
+int sub(int a, int b)
+
+{
+	return (a - b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division number
+ */
+
+int div(int a, int b)
+
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod of two integers
+ * @a: first int
+ * @b: second int
+ * Return: the final result
+ */
+
+int mod(int a, int b)
+
+{
+	return (a % b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplied result
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
