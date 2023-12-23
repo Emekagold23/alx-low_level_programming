d952026498711cafe52ed542ef04d12994914a0c
Emekagold23 <onyekachigold23@gmail.com>
2023-12-22 20:35:37 -0800

    c-hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,15 +2,13 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size: the size of the hash table
+ * @size: size of the hash table
  *
  * Return: pointer to hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
-
 {
-
 	hash_table_t *table;
 
 	if (size == 0)
